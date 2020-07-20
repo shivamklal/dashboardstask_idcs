5a37e17b04aa1bc059eabb7ea754b6618124fe44
shivlal <shivamklal@gmail.com>
2020-07-20 15:47:25 -0500

    group names coming from data :)))))))

--- src/components/Basic.jsx
+++ src/components/Basic.jsx
@@ -7,7 +7,7 @@ class Basic extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            labels: ['Group A', 'Group B', 'Group C'],
+            labels: ["", "", ""],
             datasets: [{
                 data: [23, 36, 15],
                 backgroundColor: ['red', 'blue', 'green']
@@ -15,7 +15,7 @@ class Basic extends Component{
         }
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.readData = this.readData.bind(this);
         this.readData(testdata);
     }
@@ -44,16 +44,47 @@ class Basic extends Component{
         rawFile.onreadystatechange = () => {
             if(rawFile.readyState === 4) {
                 if(rawFile.status === 200 || rawFile.status === 0) {
-                    var allText = rawFile.responseText;
-                    console.log(allText);
+                    let allText = rawFile.responseText;
                     this.setState({
                         sampleData: allText
+                    }, function () {
+                        this.setState({
+                            allDataArray: this.state.sampleData.split("\n")
+                        }, function () {
+                            console.log(this.state.allDataArray);
+                            // get unique group names
+                            let labelIndex = 0 // manages index of array of unique group names, incremented only when a new name is found
+                            let uniqueGroup = ""
+                            let currentGroup = ""
+                            for(let i = 0; i < this.state.allDataArray.length; i++){
+                                // find the first group name
+                                let endInd = this.state.allDataArray[i].indexOf(",") // position of comma to get position of end of group name
+                                currentGroup = this.state.allDataArray[i].slice(0,endInd)
+
+                                if(i === 0){ // stores the first group name
+                                    uniqueGroup = currentGroup
+                                    this.state.labels[labelIndex] = uniqueGroup
+                                }
+
+                                if(currentGroup !== uniqueGroup){
+                                    labelIndex = labelIndex + 1
+                                    uniqueGroup = currentGroup
+                                    this.state.labels[labelIndex] = uniqueGroup
+                                }
+
+                            }
+
+                            console.log(this.state.labels)
+                        });
                     });
                 }
             }
         };
         rawFile.send(null);
     }
+
+    // how to pass parameters from one function to another in reactJS
+
 }
 
 export default Basic;
